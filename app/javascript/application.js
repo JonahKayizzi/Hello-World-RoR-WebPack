a343cd44a8f291e8d930a0158149d2997d74d867
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-22 13:09:37 +0300

    Update application js file to render App and component and the static view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import store from './redux/configureStore';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
