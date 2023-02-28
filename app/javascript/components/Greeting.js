4b33744d1c656e6fb5834ae50045a918ff7d05b7
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-28 21:25:53 +0300

    Add dispatch method for greetings in Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greetings';
 
 const Greeting = () => {
   const { greeting } = useSelector((state) => state.greetings);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, []);
+
   return <h1>{greeting}</h1>;
 };
 
